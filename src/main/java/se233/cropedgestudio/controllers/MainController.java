da2b76f4c2b8ab1ae7d2c651b54d52550c735f13
Watcharapong Wanna <jaytakuz@users.noreply.github.com>
2024-10-06 14:10:58 +0700

    improve the main page layout and sensitive Feature menu that changes based on the current page

--- src/main/java/se233/cropedgestudio/controllers/MainController.java
+++ src/main/java/se233/cropedgestudio/controllers/MainController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import java.io.IOException;
 import java.net.URL;
@@ -13,8 +15,8 @@ import java.util.ResourceBundle;
 public class MainController implements Initializable {
     private static MainController instance;
 
-    @FXML
-    private VBox contentArea;
+    @FXML private VBox contentArea;
+    @FXML private Menu featuresMenu;
 
     public static MainController getInstance() {
         return instance;
@@ -24,21 +26,25 @@ public class MainController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         instance = this;
         loadView("/se233/cropedgestudio/views/MainContent.fxml");
+        updateFeaturesMenu("home");
     }
 
     @FXML
     public void handleHome() {
         loadView("/se233/cropedgestudio/views/MainContent.fxml");
+        updateFeaturesMenu("home");
     }
 
     @FXML
     public void handleDetectEdge() {
         loadView("/se233/cropedgestudio/views/EdgeDetectionView.fxml");
+        updateFeaturesMenu("edge");
     }
 
     @FXML
     public void handleCrop() {
         loadView("/se233/cropedgestudio/views/CropView.fxml");
+        updateFeaturesMenu("crop");
     }
 
     @FXML
@@ -73,5 +79,41 @@ public class MainController implements Initializable {
         alert.setHeaderText(null);
         alert.setContentText(message);
         alert.showAndWait();
+    }
+
+    private void updateFeaturesMenu(String currentPage) {
+        featuresMenu.getItems().clear();
+        MenuItem refreshItem = new MenuItem();
+        MenuItem edgeDetectionItem = new MenuItem();
+        MenuItem cropItem = new MenuItem();
+
+        switch (currentPage) {
+            case "home":
+                refreshItem.setText("Refresh Home");
+                refreshItem.setOnAction(e -> handleHome());
+                edgeDetectionItem.setText("Go to Edge Detection page");
+                edgeDetectionItem.setOnAction(e -> handleDetectEdge());
+                cropItem.setText("Go to Crop Image page");
+                cropItem.setOnAction(e -> handleCrop());
+                break;
+            case "edge":
+                refreshItem.setText("Refresh Edge Detection");
+                refreshItem.setOnAction(e -> handleDetectEdge());
+                edgeDetectionItem.setText("Go to Home page");
+                edgeDetectionItem.setOnAction(e -> handleHome());
+                cropItem.setText("Go to Crop Image page");
+                cropItem.setOnAction(e -> handleCrop());
+                break;
+            case "crop":
+                refreshItem.setText("Refresh Crop Image");
+                refreshItem.setOnAction(e -> handleCrop());
+                edgeDetectionItem.setText("Go to Home page");
+                edgeDetectionItem.setOnAction(e -> handleHome());
+                cropItem.setText("Go to Edge Detection page");
+                cropItem.setOnAction(e -> handleDetectEdge());
+                break;
+        }
+
+        featuresMenu.getItems().addAll(refreshItem, edgeDetectionItem, cropItem);
     }
 }
